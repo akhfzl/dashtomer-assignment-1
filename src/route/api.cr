a74a17aa18d48a4b2c8551d0f4e08a5d40869dcc
akhfzl <akhfaiz14@gmail.com>
2025-04-10 12:19:58 +0700

    'cheapest'

--- src/route/api.cr
+++ src/route/api.cr
@@ -9,16 +9,27 @@ get "/route" do |env|
     dest = parsed["destination"].as_s
     type = parsed["type"].as_s
 
-    json_path = Path[__DIR__] / ".." / "data" / "db.json"
+    json_path = Path[__DIR__] / ".." / "data" / "db_multi_transit.json"
     data = databases(json_path.expand)
 
+    finals = [] of Hash(String, String)
+    code = 200
+
     if type == "cheapest-direct"
-        cheapest_direct(data, orign, dest)
-    
+        finals = cheapest_direct(data, orign, dest)
     elsif type == "cheapest"
-        puts "cheapest"
-    
-    else 
-        fastest(data, orign, dest)
+        finals = cheapest(data, orign, dest)
+    elsif type == "fastest"
+        finals = fastest(data, orign, dest)
+    end 
+
+    puts ""#{finals}"
+    if finals.empty?
+        code = 400
     end
+
+    {
+        "code" => code,
+        "data" => finals
+    }.to_json
 end
--- src/route/utils.cr
+++ src/route/utils.cr
@@ -1,6 +1,7 @@
 require "../data/meta"
 require "time"
 
+# open db json
 def databases(filename : Path | String)
     data = File.read(filename)
     DB.from_json(data)
@@ -14,6 +15,7 @@ def time_parsing(sailing : Sailing) : Hash(String, Time)
     }
 end
 
+#convert concurrency
 def convert_to_eur(rate_date, rate : Rate) : Float64
     # check if eur
     currency = rate.rate_currency.downcase
@@ -29,7 +31,81 @@ def convert_to_eur(rate_date, rate : Rate) : Float64
     rate.rate.to_f / exchange
 end
 
-def cheapest_direct(data : DB, origin : String, dest : String)
+# check paths fastest or cheapest
+def explore_paths(
+  data : DB,
+  current : String,
+  dest : String,
+  path : Array(Sailing),
+  visited : Set(String),
+  total_cost_time : Float64 | Int64,
+  best_route : Array(Sailing),
+  best_duration_cost : Float64 | Int64,
+  is_fastest : Bool = false
+)
+    # count total time no more best_duration_cost
+    if current == dest && !path.empty?
+        if total_cost_time < best_duration_cost
+            best_route.clear
+            best_route.concat(path)
+            return total_cost_time
+        end
+        return best_duration_cost
+    end
+
+    data.sailings.each do |transite|
+        transite_parsing = time_parsing(transite)
+        path_parsing = time_parsing(path.first)
+
+
+        # if current point equal to continues origin port
+        next unless current == transite.origin_port
+
+        # the 2nd route ends at the requested final dest
+        next unless transite_parsing["departure_date"] > path_parsing["arrival_date"]
+
+        if is_fastest
+            # count new total time then compare with best_duration_cost
+            t_time = time_parsing(transite)
+            p_time = time_parsing(path.first)
+            new_total_cost_time = (t_time["arrival_date"] - p_time["departure_date"]).days rescue 0.0
+
+        else
+            # check rate of transite
+            rate_transite = data.rates.find { |r| r.sailing_code == transite.sailing_code }
+            next unless rate_transite 
+
+            # check rate of path (first stage)
+            rate_path = data.rates.find { |r| r.sailing_code == path.first.sailing_code }
+            next unless rate_path
+
+            # check rate date of transite and path (first stage)
+            transite_rate_date = data.exchange_rates[transite.departure_date]?
+            path_rate_date = data.exchange_rates[path.first.departure_date]?
+            new_total_cost_time = convert_to_eur(path_rate_date, rate_path) - convert_to_eur(transite_rate_date, rate_transite)
+        end
+
+        # skip if new total time more best_duration_cost
+        next if new_total_cost_time >= best_duration_cost
+
+        path.push(transite)
+        visited.add(transite.sailing_code)
+        best_duration_cost = explore_paths(data, transite.destination_port, dest, path, visited, new_total_cost_time, best_route, best_duration_cost, is_fastest)
+        path.pop
+        visited.delete(transite.sailing_code)
+
+        puts <<-INFO
+            ORIGIN - first stage #{current} - continue stage #{transite.origin_port}
+            DEST - continue stage #{transite.destination_port}
+            cost/duration #{best_duration_cost}
+        INFO
+    end
+
+    return best_duration_cost
+end
+
+# check cheapest and best direct 
+def check_cheapest_direct(data : DB, origin : String, dest : String)
     # initial
     cheapest = nil
     min_cost = Float64::INFINITY
@@ -54,9 +130,19 @@ def cheapest_direct(data : DB, origin : String, dest : String)
         end
     end
 
+    {cheapest, min_cost, best_rate}
+end
+
+# cheapest direct feature
+def cheapest_direct(data : DB, origin : String, dest : String)
+    output = [] of Hash(String, String)
+
+    # no transit but cheap
+    cheapest, min_cost, best_rate = check_cheapest_direct(data, origin, dest)
+
     # return object needed
     if cheapest && best_rate
-        {
+        output << {
             "origin_port"      => cheapest.origin_port,
             "destination_port" => cheapest.destination_port,
             "departure_date"   => cheapest.departure_date,
@@ -64,60 +150,13 @@ def cheapest_direct(data : DB, origin : String, dest : String)
             "sailing_code"     => cheapest.sailing_code,
             "rate"             => best_rate.rate,
             "rate_currency"    => best_rate.rate_currency
-        }.to_json
-    else
-        ({} of String => String).to_json
-    end
-end
-
-def explore_paths(data : DB, current : String, dest : String, path : Array(Sailing), visited : Set(String), total_time : Float64 | Int64, best_route : Array(Sailing), best_duration : Float64 | Int64)
-    # count total time no more best_duration
-    if current == dest && !path.empty?
-        if total_time < best_duration
-            best_route.clear
-            best_route.concat(path)
-            return total_time
-        end
-        return best_duration
-    end
-
-    data.sailings.each do |transite|
-        transite_parsing = time_parsing(transite)
-        path_parsing = time_parsing(path.last)
-
-
-        # if current point equal to continues origin port
-        next unless current == transite.origin_port
+        }
+    end 
     
-        puts <<-INFO
-            ORIGIN - first stage #{current} - continue stage #{transite.origin_port}
-            DEST - continue stage #{transite.destination_port}
-            continue departure_date: #{transite_parsing["departure_date"]}
-            first stage arrival_date: #{path_parsing["arrival_date"]}
-            transite.departure > path.arrival? #{transite_parsing["departure_date"] > path_parsing["arrival_date"]}
-        INFO
-
-        # the 2nd route ends at the requested final dest
-        next unless transite_parsing["departure_date"] > path_parsing["arrival_date"]
-
-        # count new total time then compare with best_duration
-        t_time = time_parsing(transite)
-        p_time = time_parsing(path.first)
-        new_total_time = (t_time["arrival_date"] - p_time["departure_date"]).days rescue 0.0
-
-        # skip if new total time more best_duration
-        next if new_total_time >= best_duration
-
-        path.push(transite)
-        visited.add(transite.sailing_code)
-        best_duration = explore_paths(data, transite.destination_port, dest, path, visited, new_total_time, best_route, best_duration)
-        path.pop
-        visited.delete(transite.sailing_code)
-    end
-
-    return best_duration
+    output
 end
 
+# fastest feature
 def fastest(data : DB, origin : String, dest : String) 
     time_direct = Float64::INFINITY
     best_direct : Sailing? = nil
@@ -140,7 +179,7 @@ def fastest(data : DB, origin : String, dest : String)
     # ---- multi-transite stage --------
     # browse sailing by origin
     data.sailings.select { |s| s.origin_port == origin }.each do |start|
-        explore_paths(data, start.destination_port, dest, [start], Set{start.sailing_code}, 0.0, best_multi_route, shortest_time)
+        explore_paths(data, start.destination_port, dest, [start], Set{start.sailing_code}, 0.0, best_multi_route, shortest_time, true)
     end
 
     # looking for rate baseon sailing code
@@ -157,8 +196,6 @@ def fastest(data : DB, origin : String, dest : String)
         bmr_f = time_parsing(best_multi_route.first)
         multi_duration = (bmr_l["arrival_date"] - bmr_f["departure_date"]).days
     end
-    
-    puts "multi-duration #{multi_duration} - best_multi_route #{best_multi_route}"
 
     if best_direct && time_direct <= multi_duration
         puts "Directly - #{time_direct}"
@@ -198,5 +235,81 @@ def fastest(data : DB, origin : String, dest : String)
         end
     end
 
-    output.to_json
+    output
+end
+
+# cheapest feature
+def cheapest(data : DB, origin : String, dest : String) 
+    # initial 
+    best_multi_route = [] of Sailing
+    best_cost_transite = Float64::INFINITY
+    multi_cost_transite = 0
+    output = [] of Hash(String, String)
+    
+    # no transit but cheap 
+    cheapest, min_cost, best_rate = check_cheapest_direct(data, origin, dest)
+
+    # ---- multi-transite stage --------
+    # browse sailing by origin
+    data.sailings.select { |s| s.origin_port == origin }.each do |start|
+        explore_paths(data, start.destination_port, dest, [start], Set{start.sailing_code}, 0.0, best_multi_route, best_cost_transite, false)
+    end
+
+    # looking for rate baseon sailing code
+    find_rate = ->(code : String) do
+        data.rates.find { |r| r.sailing_code == code }
+    end
+
+    # output final
+    output = [] of Hash(String, String)
+
+    # if best_direct available and time direct no more than multi-transit
+    unless best_multi_route.empty?
+        best_multi_route.each do |sailings|
+            # check rate each sailings
+            rate = find_rate.call(sailings.sailing_code)
+            next unless rate
+
+            rate_date = data.exchange_rates[sailings.departure_date]?
+            cost = convert_to_eur(rate_date, rate)
+          
+            multi_cost_transite += cost
+
+            rate_value = rate.try(&.rate) || 0.0
+            rate_currency = rate.try(&.rate_currency) || "N/A"
+
+            output << {
+                "origin_port"    => sailings.origin_port,
+                "destination_port" => sailings.destination_port,
+                "departure_date" => sailings.departure_date,
+                "arrival_date"   => sailings.arrival_date,
+                "sailing_code"   => sailings.sailing_code,
+                "rate"           => rate_value.to_s,
+                "rate_currency"  => rate_currency
+            }     
+        end 
+    end
+
+    puts "#{output}"
+    # directly
+    if min_cost <= multi_cost_transite 
+        puts "Directly cost - #{min_cost}"
+        {
+            "origin_port"      => cheapest ? cheapest.origin_port : "",
+            "destination_port" => cheapest ? cheapest.destination_port : "",
+            "departure_date"   => cheapest ? cheapest.departure_date : "",
+            "arrival_date"     => cheapest ? cheapest.arrival_date : "",
+            "sailing_code"     => cheapest ? cheapest.sailing_code : "",
+            "rate"             => best_rate ? best_rate.rate : "N/A",
+            "rate_currency"    => best_rate ? best_rate.rate_currency : "N/A"
+        }
+
+    # transite
+    elsif !best_multi_route.empty? && min_cost > multi_cost_transite
+        puts "Multi-Transit - #{multi_cost_transite}"
+        output
+    
+    else 
+        ([] of Hash(String, String))
+    end
 end
